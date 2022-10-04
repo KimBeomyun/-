1bcf5894a0510c6d1722a49a4accdf834291f325
김범윤 <75959977+KimBeomyun@users.noreply.github.com>
2022-10-04 17:45:45 +0900

    실습

---
+++ ImageProcessing_2018108249Doc.cpp
@@ -0,0 +1,224 @@
+﻿
+// ImageProcessing_2018108249Doc.cpp: CImageProcessing2018108249Doc 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "ImageProcessing_2018108249.h"
+#endif
+
+#include "ImageProcessing_2018108249Doc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+#include "CDownSampleDlg.h"
+// CImageProcessing2018108249Doc
+
+IMPLEMENT_DYNCREATE(CImageProcessing2018108249Doc, CDocument)
+
+BEGIN_MESSAGE_MAP(CImageProcessing2018108249Doc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CImageProcessing2018108249Doc 생성/소멸
+
+CImageProcessing2018108249Doc::CImageProcessing2018108249Doc() noexcept
+{
+	// TODO: 여기에 일회성 생성 코드를 추가합니다.
+
+}
+
+CImageProcessing2018108249Doc::~CImageProcessing2018108249Doc()
+{
+}
+
+BOOL CImageProcessing2018108249Doc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: 여기에 재초기화 코드를 추가합니다.
+	// SDI 문서는 이 문서를 다시 사용합니다.
+
+	return TRUE;
+}
+
+
+
+
+// CImageProcessing2018108249Doc serialization
+
+void CImageProcessing2018108249Doc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: 여기에 저장 코드를 추가합니다.
+	}
+	else
+	{
+		// TODO: 여기에 로딩 코드를 추가합니다.
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// 축소판 그림을 지원합니다.
+void CImageProcessing2018108249Doc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// 문서의 데이터를 그리려면 이 코드를 수정하십시오.
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// 검색 처리기를 지원합니다.
+void CImageProcessing2018108249Doc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// 문서의 데이터에서 검색 콘텐츠를 설정합니다.
+	// 콘텐츠 부분은 ";"로 구분되어야 합니다.
+
+	// 예: strSearchContent = _T("point;rectangle;circle;ole object;");
+	SetSearchContent(strSearchContent);
+}
+
+void CImageProcessing2018108249Doc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CImageProcessing2018108249Doc 진단
+
+#ifdef _DEBUG
+void CImageProcessing2018108249Doc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CImageProcessing2018108249Doc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CImageProcessing2018108249Doc 명령
+
+
+BOOL CImageProcessing2018108249Doc::OnOpenDocument(LPCTSTR lpszPathName)
+{
+	if (!CDocument::OnOpenDocument(lpszPathName))
+		return FALSE;
+
+	// TODO:  여기에 특수화된 작성 코드를 추가합니다.
+	CFile File;
+
+	File.Open(lpszPathName, CFile::modeRead | CFile::typeBinary);
+	// 파일 열기 대화상자에서 선택한 파일을 지정하고 읽기 모드 선택
+
+	// 영상의 크기 256*256, 512*512, 640*480만을 사용한다.
+	if (File.GetLength() == 256 * 256) { //RAW 파일의 크기 결정
+		m_height = 256;
+		m_width = 256;
+	}
+	else if (File.GetLength() == 512 * 512) { // RAW 파일의 크기 결정
+		m_height = 512;
+		m_width = 512;
+	}
+	else if (File.GetLength() == 640 * 480) { // RAW 파일의 크기 결정
+		m_height = 480;
+		m_width = 640;
+	}
+	else {
+		AfxMessageBox("Not Support Image Size"); // 해당 크기가 없는 경우
+		return 0;
+	}
+	m_size = m_width * m_height; //영상의 크기 계산
+
+	m_inputimage = new unsigned char[m_size];
+	// 입력 영상의 크기에 맞는 메모리 할당
+	for (int i = 0; i < m_size; i++)
+		m_inputimage[i] = 255; // 초기화
+	File.Read(m_inputimage, m_size); // 입력 영상 파일 읽기
+	File.Close(); // 파일 닫기
+	return TRUE;
+}
+
+
+BOOL CImageProcessing2018108249Doc::OnSaveDocument(LPCTSTR lpszPathName)
+{
+	CFile File;
+	CFileDialog SaveDlg(FALSE, "raw", NULL, OFN_HIDEREADONLY);
+
+	if (SaveDlg.DoModal() == IDOK) {
+		File.Open(SaveDlg.GetPathName(), CFile::modeCreate |
+		CFile::modeWrite);
+
+		File.Write(m_inputimage, m_size);
+		File.Close();	
+	}
+
+	return CDocument::OnSaveDocument(lpszPathName);
+}
+
+
+//void CImageProcessing2018108249Doc::DonDownSampling()
+//{
+//	// TODO: 여기에 구현 코드 추가.
+//}
+
+
+void CImageProcessing2018108249Doc::OnDownSapling()
+{
+	int i, j;
+	CDownSampleDlg dlg;
+	if (dlg.DoModal() == IDOK) //대화상자의 활성화 여부
+	{
+		m_Re_height = m_height / dlg.m_DownSampleRate;
+		m_Re_width = m_width / dlg.m_DownSampleRate;
+		m_Re_size = m_Re_height * m_Re_width;
+
+		m_OutputImage = new unsigned char[m_Re_size];
+
+		for (i = 0; i < m_Re_height; i++) {
+			for (j = 0; j < m_Re_width; j++) {
+				m_OutputImage[i * m_Re_width + j]
+					= m_inputimage[(i * dlg.m_DownSampleRate * m_width) + dlg.m_DownSampleRate * j];
+			}
+		}
+	}
+}
---
+++ ImageProcessing_2018108249View.cpp
@@ -0,0 +1,133 @@
+﻿
+// ImageProcessing_2018108249View.cpp: CImageProcessing2018108249View 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "ImageProcessing_2018108249.h"
+#endif
+
+#include "ImageProcessing_2018108249Doc.h"
+#include "ImageProcessing_2018108249View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CImageProcessing2018108249View
+
+IMPLEMENT_DYNCREATE(CImageProcessing2018108249View, CView)
+
+BEGIN_MESSAGE_MAP(CImageProcessing2018108249View, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+	ON_COMMAND(ID_DOWN_SAMPLING, &CImageProcessing2018108249View::OnDownSampling)
+END_MESSAGE_MAP()
+
+// CImageProcessing2018108249View 생성/소멸
+
+CImageProcessing2018108249View::CImageProcessing2018108249View() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CImageProcessing2018108249View::~CImageProcessing2018108249View()
+{
+}
+
+BOOL CImageProcessing2018108249View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CImageProcessing2018108249View 그리기
+
+void CImageProcessing2018108249View::OnDraw(CDC* pDC)
+{
+	CImageProcessing2018108249Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	int i, j;
+	unsigned char R, G, B;
+
+	for (i = 0; i < pDoc->m_height; i++) {
+		for (j = 0; j < pDoc->m_width; j++) {
+			R = G = B = pDoc->m_inputimage[i * pDoc->m_width + j];
+			pDC->SetPixel(j + 5, i + 5, RGB(R, G, B));
+		}
+	}
+	
+	for (i = 0; i < pDoc->m_Re_height; i++) {
+		for (j = 0; j < pDoc->m_Re_width; j++) {
+			R = pDoc->m_OutputImage[i * pDoc->m_Re_width + j];
+			G = B = R;
+			pDC->SetPixel(j + pDoc->m_width + 10, i + 5, RGB(R, G, B));
+		}
+	}
+}
+
+
+// CImageProcessing2018108249View 인쇄
+
+BOOL CImageProcessing2018108249View::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CImageProcessing2018108249View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CImageProcessing2018108249View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+
+// CImageProcessing2018108249View 진단
+
+#ifdef _DEBUG
+void CImageProcessing2018108249View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CImageProcessing2018108249View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CImageProcessing2018108249Doc* CImageProcessing2018108249View::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CImageProcessing2018108249Doc)));
+	return (CImageProcessing2018108249Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CImageProcessing2018108249View 메시지 처리기
+
+
+void CImageProcessing2018108249View::OnDownSampling()
+{
+	CImageProcessing2018108249Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+
+	pDoc->OnDownSapling();
+
+	Invalidate(TRUE);
+}
